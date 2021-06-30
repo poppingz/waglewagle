9f67134d24c965fc3ff5f46b8a42b2f06674ddb7
-g <-g>
2021-06-30 12:05:58 +0900

    DAO,DTO,CONTROLLER MODIFY

--- src/DAO/BoardDAO.java
+++ src/DAO/BoardDAO.java
@@ -1,10 +1,17 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import DTO.BoardDTO;
 
 
 
@@ -12,7 +19,7 @@ public class BoardDAO {
 
 	private static BoardDAO instance;
 
-	public synchronized static BoardDAO getInstance() { // 싱글톤 패턴
+	public synchronized static BoardDAO getInstance() {
 		if(instance == null) {
 			instance = new BoardDAO();
 		}
@@ -26,6 +33,50 @@ public class BoardDAO {
 		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
 		return ds.getConnection();
 	}
+	
+	public List<BoardDTO> sellectAll() throws Exception {
+		String sql = "select * from pboard";
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				ResultSet rs = pstat.executeQuery();
+				){
+			List<BoardDTO> list = new ArrayList<>();
+			while(rs.next()) {
+				int board_num = rs.getInt("board_num");
+				String id = rs.getNString("id");
+				int category = rs.getInt("category");
+				String title = rs.getNString("title");
+				String contents = rs.getNString("contents");
+				String nickname = rs.getNString("nickname");
+				Date write_date = rs.getDate("write_date");
+				int view_count = rs.getInt("view_count");
+				int report = rs.getInt("report");
+				
+				BoardDTO dto = new BoardDTO(board_num, id, category, title, contents, nickname, write_date, view_count, report);
+				list.add(dto);
+			}
+			return list;
+		}
+	}
 
+	public int modify(BoardDTO dto)throws Exception{
+		String sql = "update pboard set title=?, contents=? where board_num=?";
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				){
+			
+			pstat.setNString(1, dto.getTitle());
+			pstat.setNString(2, dto.getContents());
+			pstat.setInt(3, dto.getBoard_num());
+			
+			int result = pstat.executeUpdate();
+			
+			con.commit();
+			return result;
+			
+		}
+	}
 	
 }
--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import DAO.BoardDAO;
+import DTO.BoardDTO;
 
 
-@WebServlet("/BoardController")
+@WebServlet("*.board")
 public class BoardController extends HttpServlet {
 	
  
@@ -20,17 +22,34 @@ public class BoardController extends HttpServlet {
 		response.setContentType("text/html; charset =utf-8");
 
 		String requestURI = request.getRequestURI();
-		System.out.println("requestURI : " + requestURI);
-
 		String ctxPath = request.getContextPath();
-		System.out.println("ctxPath : " + ctxPath);
-
 		String url = requestURI.substring(ctxPath.length());
-		System.out.println("url : " + url);
 
-
-		BoardDAO dao = BoardDAO.getInstance();
-	
+		try {
+			BoardDAO dao = BoardDAO.getInstance();
+			
+			if(url.contentEquals("/select.board")) {
+				List<BoardDTO> list = dao.sellectAll();
+				request.setAttribute("BoardList", list);
+				request.getRequestDispatcher("Board/BoardList").forward(request, response);
+				
+			}else if(url.contentEquals("/insert.board")) {
+				
+			}else if(url.contentEquals("/modify.board")) {
+				int board_num = (Integer.parseInt(request.getParameter("board_num")));
+				String title = request.getParameter("title");
+				String contents = request.getParameter("contents");
+				int result = dao.modify(new BoardDTO(board_num,title,contents));
+				
+				request.setAttribute("result", result);
+				response.sendRedirect("index.jsp");
+				
+			}else if(url.contentEquals("/delete.board")) {
+				
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	
