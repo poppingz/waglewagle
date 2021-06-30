7d44bf2d5ae0aa1963d7c845aae35fb55cea55e9
cbh <choi@naver.com>
2021-06-30 12:50:35 +0900

    controller,DAO added

--- src/DAO/BoardDAO.java
+++ src/DAO/BoardDAO.java
@@ -1,6 +1,8 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -26,6 +28,77 @@ public class BoardDAO {
 		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
 		return ds.getConnection();
 	}
+	
+	
+	public int insert(String id, int category , String title, String contents, String nickname) throws Exception {
+		String sql = "insert into pboard values(board_num.nextval, ?, ?, ?, ?, ?, sysdate, 0, 0)";
+		try (
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				){
+			pstat.setString(1, id);
+			pstat.setInt(2, category);
+			pstat.setString(3, title);
+			pstat.setString(4, contents);
+			pstat.setString(5, nickname);		
+			int result = pstat.executeUpdate();
+			con.commit();
+			return result;
+		}
+	}
 
+	
+
+	
+	public int deleteBoard(int board_num) throws Exception{
+		String sql = "delete from pboard where board_num = ?";
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				){
+			pstat.setInt(1, board_num);
+
+			int result = pstat.executeUpdate();
+			con.commit();
+			return result;
+		}
+	}
+
+	
+	public void view_countPlus(int board_num) throws Exception{ // view_count 증가
+		int view_count = this.view_count(board_num);
+		String sql = "update pboard set view_count = ? where board_num = ?";
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				){
+			pstat.setInt(1, view_count + 1);
+			pstat.setInt(2, board_num);
+
+			pstat.executeUpdate();
+			con.commit();
+		}
+	}
+
+	public int view_count(int board_num) throws Exception { // view_count 증가를 위한 view_count
+		String sql = "select view_count from pboard where board_num = ?";
+		try(
+				Connection con = this.getConnection();		
+				PreparedStatement pstat = con.prepareStatement(sql);		
+				)
+		{
+			pstat.setInt(1, board_num);
+			try(
+					ResultSet rs = pstat.executeQuery();
+					){
+				rs.next();
+				int view_count = rs.getInt("view_count");
+
+				return view_count;
+			}
+		}
+	}
+	
+	
 	
 }
--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -9,12 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import DAO.BoardDAO;
+import kh.mvc.dto.MemberDTO;
 
 
-@WebServlet("/BoardController")
+@WebServlet("*.board")
 public class BoardController extends HttpServlet {
-	
- 
+
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset =utf-8");
@@ -30,10 +31,38 @@ public class BoardController extends HttpServlet {
 
 
 		BoardDAO dao = BoardDAO.getInstance();
-	
+
+
+		try {
+			
+             // 글 쓰기
+			if(url.contentEquals("/boardWrite.board")) {
+				MemberDTO dto = (MemberDTO)request.getSession().getAttribute("login");
+				int category = Integer.parseInt(request.getParameter("category"));
+				String title = request.getParameter("title");
+				String contents = request.getParameter("contents");
+				String nickname = request.getParameter("nickname");
+
+				int result = dao.insert(dto.getId(), category, title, contents, nickname);
+
+				request.setAttribute("result", result);
+				
+				response.sendRedirect(ctxPath +"/boardList.board");
+           
+				// 글 삭제
+			}else if(url.contentEquals("/boardDelete.board")) {
+				int board_num = Integer.parseInt(request.getParameter("board_num"));
+				dao.deleteBoard(board_num);
+				response.sendRedirect(ctxPath +"/boardList.board");
+			}
+
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
